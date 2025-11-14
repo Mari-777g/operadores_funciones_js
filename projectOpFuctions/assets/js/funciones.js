611860594ac2cb344744c60dab4dad5e7ba95508
Mariana Avirama <mariana.avirama07@gmail.com>
2025-11-14 18:12:24 -0500

    adelanto trabajo en clase

---
+++ projectOpFuctions/assets/js/funciones.js
@@ -0,0 +1,6 @@
+/**
+ * Funciones
+ */
+function myFuction(){
+    document.getElementById("demo3").innerHTML = "Mariana Avirama";
+}
---
+++ projectOpFuctions/assets/js/operadores.relacionales.js
@@ -0,0 +1,5 @@
+/**
+ * Operadores Relacionales
+ */
+let x = 5;
+document.getElementById("demo1").innerHTML = (x >=8);   //false
